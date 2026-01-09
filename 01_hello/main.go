b4d77ea8b97800087b882ac135f1c4d68b1fb260
samian04 <samiansikdar04@gmail.com>
2026-01-09 11:44:29 +0600

    jan9

--- 01_hello/main.go
+++ 01_hello/main.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// Declare a variable to store the user's name
-	var name string
+	// A map of student grades
+	studentGrades := map[string]string{
+		"John":  "B+",
+		"Emma":  "A-",
+		"Sarah": "A",
+		"Mike":  "C",
+	}
 	
-	// Display a prompt for the user
-	fmt.Print("Enter your name: ")
-	
-	// TODO: Use fmt.Scanln() to read the user's input into the 'name' variable
-	fmt.Scanln(&name)
-	// Display a greeting using the name entered
-	fmt.Printf("Hello, %s! fuck me harder.\n", name)
+	// TODO: Access and store Emma's grade in a variable called emmaGrade
+	emmaGrade := studentGrades["Emma"]
+	// TODO: Print Emma's grade with a message like: "Emma's grade is: A-"
+	fmt.Printf("Emma's grade is: %s\n",emmaGrade)
 }
