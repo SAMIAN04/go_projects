a2e67a6c54672a0e464d0579cedfe66ecb19331a
samian04 <samiansikdar04@gmail.com>
2026-01-23 20:24:56 +0600

    jan23

--- 01_hello/main.go
+++ 01_hello/main.go
@@ -2,55 +2,37 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+
+	
 )
 
-// TODO: Define the Account struct here
-type Account struct {
-	Name    string
-	Balance float64
-}
-
-// TODO: Define the deposit method with pointer receiver here
-func (a *Account) deposit(transactionAmount float64) {
-	a.Balance += transactionAmount
-}
-
-// TODO: Define the withdraw method with pointer receiver here
-func (a *Account) withdraw(transactionAmount float64) {
-    a.Balance -= transactionAmount
-}
-
-// TODO: Define the displayBalance method with value receiver here
-   func (a Account) displayBalance()  {
-    fmt.Printf("Account: %s, Balance: $%.2f\n", a.Name,a.Balance)
-   }
 func main() {
 	// Read input
-	var name string
-	var initialBalanceStr string
-	var transactionAmountStr string
+	var action string
+	fmt.Scanln(&action)
 
-	fmt.Scanln(&name)
-	fmt.Scanln(&initialBalanceStr)
-	fmt.Scanln(&transactionAmountStr)
-
-	// Convert string inputs to appropriate types
-	initialBalance, _ := strconv.ParseFloat(initialBalanceStr, 64)
-	transactionAmount, _ := strconv.ParseFloat(transactionAmountStr, 64)
-
-	// TODO: Create an Account instance
-account := Account{
-	Name:    name,
-	Balance: initialBalance,
+	// TODO: Define your MediaPlayer interface here
+	type MediaPlayer interface {
+		Play() string
+		Pause() string
+		Stop() string
+	}
+	// TODO: Write your code below to handle the action and print the required output
+switch action {
+case "play": {
+	fmt.Println("MediaPlayer interface requires: Play() string")
 }
-
-// TODO: Display initial balance, perform deposit, display balance, perform withdrawal, display final balance
-account.displayBalance()
-account.deposit(transactionAmount)
-account.displayBalance()
-account.withdraw(transactionAmount)
-account.displayBalance()
+case "pause" :{
+		fmt.Println("MediaPlayer interface requires: Pause() string")
 
 }
+case "stop" :{
+	fmt.Println("MediaPlayer interface requires: Stop() string")
+
+}
+}
+	
+}
+	// Print the result
+	// Remember to print in format: "MediaPlayer interface requires: [MethodName]() string"
 
