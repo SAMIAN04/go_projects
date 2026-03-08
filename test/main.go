0ba1dee4062d10dde4131d65b7ed8fc157c2e185
samian04 <samiansikdar04@gmail.com>
2026-03-08 11:44:46 +0600

    march 8

--- test/main.go
+++ test/main.go
@@ -23,41 +23,65 @@ func main() {
 	// Declare maps without initialization (they will be nil)
 	var scores map[string]int
 	var grades map[string]int
-	fmt.Println(scores, grades)
+
 	// Split operations by comma
 	operationsParts := strings.Split(operationsStr, ",")
 	operations := []Opatrations{}
 
-	
 	for _, operationParts := range operationsParts {
 		operation := strings.Split(operationParts, ":")
 
 		if len(operation) == 2 {
 			task := operation[0]
 			types := operation[1]
-					    operations = append(operations,Opatrations{Task: task,Type: types,} )
+			operations = append(operations, Opatrations{Task: task, Type: types})
 
 		} else if len(operation) == 4 {
 			task := operation[0]
 			types := operation[1]
 			name := operation[2]
-		    score ,_:= strconv.Atoi(operation[3])
-		    operations = append(operations,Opatrations{Task: task,Type: types,score: score,Name: name} )
-		} 
-		
-		//    task := operation[0]
-		//    types := operation[1]
-		//    name := operation[2]
-		//    score ,_:= strconv.Atoi(operation[3])
-		//    operations = append(operations,Opatrations{Task: task,Type: types,score: score,Name: name} )
-
+			score, _ := strconv.Atoi(operation[3])
+			operations = append(operations, Opatrations{Task: task, Type: types, score: score, Name: name})
+		}
 	}
-	fmt.Println(operations)
+	for _, operation := range operations {
+		switch operation.Task {
+		case "init":
+			{
+				if operation.Type == "scores" {
+					Mapinit(scores, operation.Type)
+				} else if operation.Type == "grades" {
+					Mapinit(grades, operation.Type)
+
+				}
+
+			}
+		case "add" :{
+			if operation.Type == "scores" {
+					add(scores,operation.Type,operation.Name,operation.score)
+				} else if operation.Type == "grades" {
+					add(grades,operation.Type,operation.Name,operation.score)
+
+				}
+		}
+		}
+	}
 
 }
 
-func Mapinit(scores map[string]int, oparetions []Opatrations)  {
-	if scores  {
-		
+func Mapinit(scores map[string]int, Type string) {
+	if scores == nil {
+		scores = make(map[string]int)
 	}
+	fmt.Printf("Initialized map %s\n", Type)
+}
+
+func add (scores map[string]int,Type string, name string,score int ){
+   if scores == nil {
+	fmt.Printf("Cannot add to nil map %s\n", Type)
+   }else {
+	fmt.Printf("Added %s:%d to %s\n", name,score,Type)
+	scores[name] = score
+	
+   }
 }
